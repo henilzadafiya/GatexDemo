d108406b95358658e4ad8db70f267ab6c7dbfa94
Henil Zadafiya <henil.zadafiya@anant.co.in>
2024-11-27 18:49:45 +0530

    quest api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:gatexdemo/favrite_controller.dart';
 import 'package:gatexdemo/post_controller.dart';
 import 'package:gatexdemo/post_model.dart';
+import 'package:gatexdemo/quest_modal.dart';
+import 'package:gatexdemo/qutesApiController.dart';
 import 'package:gatexdemo/user_controller.dart';
 import 'package:gatexdemo/user_model.dart';
 import 'package:get/get.dart';
@@ -22,7 +24,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  PostController controller = Get.put(PostController());
+  // PostController controller = Get.put(PostController());
+  QutesApiController controller = Get.put(QutesApiController());
 
   @override
   void initState() {
@@ -30,34 +33,69 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     controller.getHttp();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Get tutarioal')),
-      body: FutureBuilder(
-        future: controller.getHttp(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            if (snapshot.hasData) {
-              Map<String ,dynamic> data = snapshot.data;
-              log("messagemessage : ${data.keys}");
-              List post = data['posts'];
-              log('====${data}');
-              return ListView.builder(
-                itemCount: data.length, itemBuilder: (context, index) {
-                return Card(child: ListTile(
-                  title: Text('${post[index]['title']}'),
-                  subtitle: Text('${post[index]['body']}'),
-                  leading: Text('${post[index]['reactions']['likes']}'),
-                ),);
-              },);
-            } else {
-              return Text("NO DATA");
-            }
-          }
-        },
-      ),);
+        appBar: AppBar(title: Text('Get tutarioal')),
+        body: Center(
+          child: FutureBuilder(
+            future: controller.getHttp(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else {
+                if (snapshot.hasData) {
+                  Map<String, dynamic> m = snapshot.data as Map<String, dynamic>;
+                  List data = m['quotes'];
+                  print(m.keys);
+                  print(data);
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text('${data[index]['quote']}'),
+                          leading: Text('${data[index]['id']}'),
+                          subtitle: Text('${data[index]['author']}'),
+                        ),
+                      );
+                    },
+                  );
+                }else {
+                  return Text("No Data");
+                }
+              }
+            },
+          ),
+        )
+
+        // post api in coma
+        // FutureBuilder(
+        //   future: controller.getHttp(),
+        //   builder: (context, snapshot) {
+        //     if (snapshot.connectionState == ConnectionState.waiting) {
+        //       return Center(child: CircularProgressIndicator());
+        //     } else {
+        //       if (snapshot.hasData) {
+        //         Map<String ,dynamic> data = snapshot.data;
+        //         log("messagemessage : ${data.keys}");
+        //         List post = data['posts'];
+        //         log('====${data}');
+        //         return ListView.builder(
+        //           itemCount: data.length, itemBuilder: (context, index) {
+        //           return Card(child: ListTile(
+        //             title: Text('${post[index]['title']}'),
+        //             subtitle: Text('${post[index]['body']}'),
+        //             leading: Text('${post[index]['reactions']['likes']}'),
+        //           ),);
+        //         },);
+        //       } else {
+        //         return Text("NO DATA");
+        //       }
+        //     }
+        //   },
+        // ),
+        );
   }
 }
--- lib/qutesApiController.dart
+++ lib/qutesApiController.dart
@@ -1,5 +1,20 @@
-import 'package:get/get.dart';
+import 'dart:convert';
+import 'dart:developer';
 
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 class QutesApiController extends GetxController{
+
+ Future getHttp() async {
+   var url = Uri.https('dummyjson.com','quotes');
+   var response = await http.get(url);
+   // log(response.body);
+   if(response.statusCode == 200) {
+     final Map<String, dynamic> data = jsonDecode(response.body);
+     // log('${jsonEncode(data)}');
+     // print(data['quotes']);
+     return data;
+   }
+ }
 
 }
